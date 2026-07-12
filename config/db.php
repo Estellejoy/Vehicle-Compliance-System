c2cb0afc6d89ae82fbcecbb622a8aad87b2fb7f0
Jemima <jemima.moye@strathmore.edu>
2026-07-12 15:28:01 +0300

    feat: set default time zone for MySQL and PHP configurations

--- config/db.php
+++ config/db.php
@@ -17,6 +17,7 @@ $options = [
     PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
     PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
     PDO::ATTR_EMULATE_PREPARES   => false,
+    PDO::MYSQL_ATTR_INIT_COMMAND => "SET time_zone = '+03:00'",
 ];
 
 try {
